b31bf01de8a833bfd82eb53482fe4508efcff8ed
ClippedALT <135581346+ClippedALT@users.noreply.github.com>
2024-05-26 17:00:50 -0500

    Update uv.config.js

--- public/uv/uv.config.js
+++ public/uv/uv.config.js
@@ -1,7 +1,5 @@
-// This file overwrites the stock UV config.js
-
 self.__uv$config = {
-  prefix: "/uv/service/",
+  prefix: "/pa/",
   bare: "/bare/",
   encodeUrl: Ultraviolet.codec.xor.encode,
   decodeUrl: Ultraviolet.codec.xor.decode,
