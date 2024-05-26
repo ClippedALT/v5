1af72a7341d68a87b329ef78586bd91233b0f0ec
ClippedALT <135581346+ClippedALT@users.noreply.github.com>
2024-05-26 18:49:38 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,66 +1,21 @@
-import { createBareServer } from "@tomphttp/bare-server-node";
-import express from "express";
-import http from "http";
-import { dirname, join } from "path";
-import { hostname } from "node:os";
-import { fileURLToPath } from "url";
-import compression from 'compression';
-import chalk from 'chalk';
-import 'dotenv/config';
-import path from "node:path";
-
-let port = parseInt(process.env.PORT || "");
-if (isNaN(port)) port = 8080;
+const { createBareServer } = require("@tomphttp/bare-server-node");
+const express = require("express");
+const { createServer } = require("node:http");
+const { uvPath } = require("@titaniumnetwork-dev/ultraviolet");
+const { hostname } = require("node:os");
 
 const bare = createBareServer("/bare/");
 const app = express();
 
-const __dirname = process.cwd();
+app.use(express.static("/public"));
+app.use("/pa/", express.static(uvPath));
 
-app.get('/', (req, res) => {
-  res.sendFile(path.join(process.cwd(), '/public/index.html'));
-});
-app.get('/english', (req, res) => {
-  res.sendFile(path.join(process.cwd(), '/public/g.html'));
-});
-app.get('/science', (req, res) => {
-  res.sendFile(path.join(process.cwd(), '/public/a.html'));
-});
-app.get('/math', (req, res) => {
-  res.sendFile(path.join(process.cwd(), '/public/p.html'));
-});
-app.get('/app', (req, res) => {
-  res.sendFile(path.join(process.cwd(), '/public/q.html'));
-});
-app.get('/@', (req, res) => {
-  res.sendFile(path.join(process.cwd(), '/public/s.html'));
-});
-app.get('/tabs', (req, res) => {
-  res.sendFile(path.join(process.cwd(), '/public/test.html'));
+app.get('*', function (req, res) {
+  res.send('404');
+  res.sendFile(join("../public", "404.html"));
 });
 
-app.get("/search=:query", async (req, res) => {
-  const { query } = req.params;
-
-  const response = await fetch(
-    `http://api.duckduckgo.com/ac?q=${query}&format=json`
-  ).then((apiRes) => apiRes.json());
-
-  res.send(response);
-});
-
-app.use(compression());
-app.use(express.static(process.cwd() + "/public/"));
-app.use("/public/uv/", express.static(process.cwd() + "/uv/"));
-
-app.use((req, res) => {
-  res.status(404);
-  res.send(
-    `404`
-  );
-});
-
-const server = http.createServer();
+const server = createServer();
 
 server.on("request", (req, res) => {
   if (bare.shouldRoute(req)) {
@@ -78,14 +33,18 @@ server.on("upgrade", (req, socket, head) => {
   }
 });
 
+let port = parseInt(process.env.PORT || "");
+
+if (isNaN(port)) port = 8080;
+
 server.on("listening", () => {
   const address = server.address();
-  console.log("PLEXILE ARCADE listening on:");
+
+  console.log("Listening on:");
   console.log(`\thttp://localhost:${address.port}`);
   console.log(`\thttp://${hostname()}:${address.port}`);
   console.log(
-    `\thttp://${
-      address.family === "IPv6" ? `[${address.address}]` : address.address
+    `\thttp://${address.family === "IPv6" ? `[${address.address}]` : address.address
     }:${address.port}`
   );
 });
