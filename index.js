a21b7e82bbd169c5143ffd3aec103da094e9887d
ClippedALT <135581346+ClippedALT@users.noreply.github.com>
2024-05-26 19:06:14 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,64 +1,28 @@
-const { createBareServer } = require("@tomphttp/bare-server-node");
-const express = require("express");
-const { createServer } = require("node:http");
-const { uvPath } = require("@titaniumnetwork-dev/ultraviolet");
-const { hostname } = require("node:os");
+const createServer = require('@tomphttp/bare-server-node').default;
+const http = require('http');
+const nodeStatic = require('node-static');
 
-const bare = createBareServer("/bare/");
-const app = express();
+const bare = createServer('/bare/');
+const serve = new nodeStatic.Server('public/');
 
-app.use(express.static("/public"));
-app.use("/pa/", express.static(uvPath));
+const server = http.createServer();
 
-app.get('*', function (req, res) {
-  res.send('404');
-  res.sendFile(join("../public", "404.html"));
+server.on('request', (req, res) => {
+    if (bare.shouldRoute(req)) {
+        bare.routeRequest(req, res);
+    } else {
+        serve.serve(req, res);
+    }
 });
 
-const server = createServer();
-
-server.on("request", (req, res) => {
-  if (bare.shouldRoute(req)) {
-    bare.routeRequest(req, res);
-  } else {
-    app(req, res);
-  }
+server.on('upgrade', (req, socket, head) => {
+    if (bare.shouldRoute(req, socket, head)) {
+        bare.routeUpgrade(req, socket, head);
+    } else {
+        socket.end();
+    }
 });
-
-server.on("upgrade", (req, socket, head) => {
-  if (bare.shouldRoute(req)) {
-    bare.routeUpgrade(req, socket, head);
-  } else {
-    socket.end();
-  }
-});
-
-let port = parseInt(process.env.PORT || "");
-
-if (isNaN(port)) port = 8080;
-
-server.on("listening", () => {
-  const address = server.address();
-
-  console.log("Listening on:");
-  console.log(`\thttp://localhost:${address.port}`);
-  console.log(`\thttp://${hostname()}:${address.port}`);
-  console.log(
-    `\thttp://${address.family === "IPv6" ? `[${address.address}]` : address.address
-    }:${address.port}`
-  );
-});
-
-process.on("SIGINT", shutdown);
-process.on("SIGTERM", shutdown);
-
-function shutdown() {
-  console.log("SIGTERM signal received: closing HTTP server");
-  server.close();
-  bare.close();
-  process.exit(0);
-}
 
 server.listen({
-  port,
+    port: process.env.PORT || 8080,
 });
