37b8a4e8664cbc69509fab3342052372ee9233e2
ClippedALT <135581346+ClippedALT@users.noreply.github.com>
2024-05-26 19:49:56 -0500

    Update index.mjs

--- index.mjs
+++ index.mjs
@@ -1,7 +1,7 @@
 import createServer from '@tomphttp/bare-server-node';
 import http from 'http';
 import nodeStatic from 'node-static';
-
+const port = process.env.PORT || 8080;
 
 const bare =  createServer('/bare/');
 const serve = new nodeStatic.Server('public/');
@@ -9,21 +9,26 @@ const serve = new nodeStatic.Server('public/');
 const server = http.createServer();
 
 server.on('request', (req, res) => {
-    if (bare.shouldRoute(req)) {
-		bare.routeRequest(req, res);
-	} else {
-		serve.serve(req, res);
-	}
+  if (bare.shouldRoute(req)) {
+    bare.routeRequest(req, res);
+  } else {
+    serve.serve(req, res);
+  }
 });
 
 server.on('upgrade', (req, socket, head) => {
-	if (bare.shouldRoute(req, socket, head)) {
-		bare.routeUpgrade(req, socket, head);
-	}else{
-		socket.end();
-	}
+  if (bare.shouldRoute(req, socket, head)) {
+    bare.routeUpgrade(req, socket, head);
+  }else{
+    socket.end();
+  }
 });
 
+server.listen({
+  port: port,
+});
+
+console.log(`Listening on http://localhost:${port}`)
 server.listen({
 	port: process.env.PORT || 8080,
 });
