785dc00e555324b987dec8fff9d7b1eb6bf670c7
ClippedALT <135581346+ClippedALT@users.noreply.github.com>
2024-05-26 19:07:18 -0500

    Update sw.js

--- public/sw.js
+++ public/sw.js
@@ -1,17 +1,9 @@
-importScripts("./uv/uv.bundle.js");
-importScripts("./uv/uv.config.js");
-importScripts("./uv/uv.sw.js");
+importScripts('./uv/uv.sw.js');
 
-const uv = new UVServiceWorker();
+const sw = new UVServiceWorker();
 
-self.addEventListener("fetch", (event) => {
-  event.respondWith(
-    (async function () {
-      if (event.request.url.startsWith(location.origin + "/service/")) {
-        return await uv.fetch(event);
-      }
-
-      return await fetch(event.request);
-    })()
-  );
-});
+self.addEventListener('fetch', event =>
+    event.respondWith(
+        sw.fetch(event)
+    )
+);
