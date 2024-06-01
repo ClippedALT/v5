f9268e0abfa2a6ffc16fc439e2c1a0bd60e313ee
ClippedALT <135581346+ClippedALT@users.noreply.github.com>
2024-06-01 16:10:08 -0500

    Update index.js

--- public/index.js
+++ public/index.js
@@ -1,22 +1,20 @@
-const form = document.querySelector('form');
-const input = document.querySelector('input');
+"use strict";
+const form = document.getElementById("form");
 
-form.addEventListener('submit', async event => {
-    event.preventDefault();
-    window.navigator.serviceWorker.register('./sw.js', {
-        scope: __uv$config.prefix
-    }).then(() => {
-        let url = input.value.trim();
-        if (!isUrl(url)) url = 'https://www.google.com/search?q=' + url;
-        else if (!(url.startsWith('https://') || url.startsWith('http://'))) url = 'http://' + url;
+const address = document.getElementById("input");
 
+form.addEventListener("submit", async (event) => {
+  event.preventDefault();
 
-            sessionStorage.setItem("url", __uv$config.prefix + __uv$config.encodeUrl(url))
+  try {
+    await registerSW();
+  } catch (err) {
+    console.log(err);
+    throw err;
+  }
+
+  const url = search(address.value, "https://www.google.com/search?q=%s");
+
+  sessionStorage.setItem("url", __uv$config.prefix + __uv$config.encodeUrl(url))
   location.href = "/go.html"
-    });
 });
-
-function isUrl(val = ''){
-    if (/^http(s?):\/\//.test(val) || val.includes('.') && val.substr(0, 1) !== ' ') return true;
-    return false;
-};
