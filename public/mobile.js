d424c7bc948f921c6e4d3af280b9b66b803ce6a2
ClippedALT <135581346+ClippedALT@users.noreply.github.com>
2024-06-07 15:05:43 -0500

    Update mobile.js

--- public/mobile.js
+++ public/mobile.js
@@ -1,7 +1,3 @@
-function isMobileDevice() {
-    return (/android|webos|iphone|ipad|ipod|blackberry|iemobile|opera mini/i.test(navigator.userAgent.toLowerCase()));
-}
-
-if (isMobileDevice()) {
-    window.location.href = "/mobile.html";
-}
+  document.addEventListener("DOMContentLoaded", function() {
+            document.body.style.opacity = 1;
+        });
